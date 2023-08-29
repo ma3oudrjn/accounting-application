2e77a02571354171d11335c5744b7ed6da6f151b
masoud <masoudrng2@gmail.com>
2023-08-29 17:40:35 +0330

    database config

--- Server.js
+++ Server.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+//database
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://localhost:27017/test`).then(()=>{
+        console.log("Conected To Database");
+    }),error => {
+    console.log('Could not connect to database : ' + error)
+  }
+
 
 
 
