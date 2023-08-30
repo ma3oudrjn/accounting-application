816cd397135f8b2ebbffda1ada640c0356217e99
masoud <masoudrng2@gmail.com>
2023-08-30 14:03:41 +0330

    joi

--- model/product.js
+++ model/product.js
@@ -1,4 +1,11 @@
 const express = require('express')
-const qpp = express()
+const app = express()
+const _ = require('joi')
 const mongoose = require('mongoose')
+const productSchema = new mongoose.Schema({
+name: _.string().min(3),
+comi
 
+
+
+})
