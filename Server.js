46433deb5f030b0fb2ae7f3fe98e4f29f945a846
masoud <masoudrng2@gmail.com>
2023-09-02 13:16:06 +0330

    midelwere err

--- Server.js
+++ Server.js
@@ -1,24 +1,36 @@
-const { createLoginCallbackHandler } = require('@okta/oidc-middleware/src/connectUtil');
 const express = require('express')
 const app = express()
 app.use(express.json())
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const product = require('./routes/productRoutes')
 //database
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://localhost:27017/test`).then(()=>{
+mongoose.connect(`mongodb://localhost:27017/test`,{useNewUrlParser:true},{useFindAndModify:false},{useCreateIndex:true},{useUnifiedTopology:true}
+).then(()=>{
         console.log("Conected To Database");
     }),error => {
     console.log('Could not connect to database : ' + error)
   }
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
 //home page request
   app.get('/',(req,res)=>{
 res.send("wellcome to the first page")
  console.log("req:","home page request");
   })
 
-  app.ge
-
+  app.use(cors());
+  app.use('/product', product)
 
 
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,8 +3,19 @@ const app = express()
 pRouter=express.Router()
 
 const mongoose = require('mongoose')
-const productSchema = require('/model/product')
+const productSchema = require('../model/product')
+// const { date } = require('joi')
 
+pRouter.post("/add/product", (req, res, next) => {
+    productSchema.create(req.body, (error, data) => {
+      if (error) {
+        return next(error);
+      } else {
+        console.log(data);
+        res.json(data);
+      }
+    });
+  });
 
 
 
