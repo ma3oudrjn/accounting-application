790b42bb8024620671dee4513902602ee6054ca7
masoud <masoudrng2@gmail.com>
2023-09-02 08:49:50 +0330

    admin schema added

--- model/product.js
+++ model/product.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const _ = require('joi')
 const mongoose = require('mongoose')
+
 const productSchema = new mongoose.Schema({
 name: _.string().min(3).required(),
 price: _.number().required(),
 serialNumber: _.string().required(),
 numberOfInventory: _.number(),
 description: _.string().required(),
-imageUrl: _.string()
-
-
-
+imageUrl: _.string(),
+isLive: _.boolean().required()
 })
+
+module.exports = mongoose.model(product,productSchema)
