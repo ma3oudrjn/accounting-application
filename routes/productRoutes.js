ea983eb9e7611cab7ebebfb3dae925cf4f6d105f
masoud <masoudrng2@gmail.com>
2023-09-02 09:48:50 +0330

    Router files

---
+++ routes/productRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+pRouter=express.Router()
+
+const mongoose = require('mongoose')
+const productSchema = require('/model/product')
+
+
+
+
+
+
+
+
+module.exports=pRouter
