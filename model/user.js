b61d996b93bb6edc3703adb1d9f3cbd82e8a45d5
masoud <masoudrng2@gmail.com>
2023-09-04 12:10:08 +0330

    add cart model

--- model/user.js
+++ model/user.js
@@ -4,10 +4,9 @@ const joi = require('joi')
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-"name": String,
-"id": Number,
-"wallet": Number,
-"Cart": [{}],
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
 })
 
 module.exports = mongoose.model("users",userSchema)
