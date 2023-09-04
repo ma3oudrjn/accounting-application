bcf5f7b1750187cb14c7c9970b455801fd71f1fc
masoud <masoudrng2@gmail.com>
2023-09-04 15:33:45 +0330

    some change

--- model/user.js
+++ model/user.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    wallet: { type: Number, required: true}
 })
 
 module.exports = mongoose.model("users",userSchema)
